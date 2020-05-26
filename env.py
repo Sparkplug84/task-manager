5af63f3cb3f4affcb71a40c640fb92de4f102c63
Mark McClean <mcclean_mark_02@hotmail.com>
2020-05-26 17:33:58 +0000

    add task, edit task, add category and edit category pages added

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault("MONGODB_NAME", "task_manager")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://root:Istanbul05@myfirstcluster-nzawj.mongodb.net/task_manager?retryWrites=true&w=majority")
